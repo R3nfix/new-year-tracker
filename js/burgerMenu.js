196844f8f6508246e6ce22d13e3fc9e26fb469eb
R3nfix <a.l3v279@gmail.com>
2026-05-03 00:43:25 +0300

    added animation

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -5,9 +5,14 @@ const burgerMenuButtonElement = document.querySelector('[data-js-menu]');
 const burgerMenuButtonCloseElement = document.querySelector('[data-js-close]');
 
 burgerMenuButtonElement.addEventListener('click', () => {
+    menuContainer.style.display = 'flex';
     menuContainer.classList.remove('menu-hide');
 });
 
 burgerMenuButtonCloseElement.addEventListener('click', () => {
     menuContainer.classList.add('menu-hide');
+
+    setTimeout(() => {
+        menuContainer.style.display = 'none'; 
+    }, 400);
 });
