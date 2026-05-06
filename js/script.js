224c98ba3ac4a17a918a8e0ca0160b071ddc8672
R3nfix <a.l3v279@gmail.com>
2026-05-06 22:09:37 +0300

    added script.js and adaptation for popup

---
+++ js/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import { themeHandler, toggleDarkTheme } from "./theme.js";
+
+const days = document.querySelector('[data-js-days]');
+const hours = document.querySelector('[data-js-hours]');
+const minutes = document.querySelector('[data-js-minutes]');
+const seconds = document.querySelector('[data-js-seconds]');
+
+// Переменные для отсчёта до НГ
+const currentYear = new Date().getFullYear();
+const newYear = new Date(`1 Jan ${currentYear + 1} 00:00:00`);
+
+// Переменные для отсчёта после НГ
+const currentDate = new Date();
+const beginningYear = new Date('1 Jan 2026 00:00:00');
+
+function countdownNewYear() {
+    const currentDate = Date.now();
+    const difference = newYear - currentDate;
+
+    const d = Math.floor((difference / 1000 / 60 / 60 / 24));
+    const h = Math.floor((difference / 1000 / 60 / 60) % 24);
+    const m = Math.floor((difference / 1000 / 60 ) % 60)
+    const s = Math.floor((difference / 1000) % 60);
+
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+}
+
+function countdownAfterNewYear() {
+    const currentDate = Date.now();
+    const difference = currentDate - beginningYear;
+
+    const d = Math.floor((difference / 1000 / 60 / 60 / 24));
+    const h = Math.floor((difference / 1000 / 60 / 60) % 24);
+    const m = Math.floor((difference / 1000 / 60 ) % 60)
+    const s = Math.floor((difference / 1000) % 60);
+
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+}
+
+if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
+    countdownNewYear();
+    setInterval(countdownNewYear, 1000);
+} else if (window.location.pathname.includes('days-passed.html')) {
+    countdownAfterNewYear();
+    setInterval(countdownAfterNewYear, 1000);
+}
